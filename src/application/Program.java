89c037aa426bdb5af35c1b52e873e7a6dcb1cdab
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2020-10-01 22:06:18 -0300

    Good solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,47 +6,49 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args){
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Scanner dado = new Scanner(System.in);
 
-		System.out.print("Enter Room number: ");
-		int number = dado.nextInt();
-		dado.nextLine();
-		System.out.print("Check-in date: (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(dado.nextLine());
-		System.out.print("Check-out date: (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(dado.nextLine());
-
-		if (!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		}
-
-		else {
+		try {
+		
+			System.out.print("Enter Room number: ");
+			int number = dado.nextInt();
+			dado.nextLine();
+			System.out.print("Check-in date: (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(dado.nextLine());
+			System.out.print("Check-out date: (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(dado.nextLine());
+	
+		
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
 			System.out.println();
-
+	
 			System.out.println("Enter data to update the reservation: ");
 			System.out.print("Check-in date: (dd/MM/yyyy): ");
 			checkIn = sdf.parse(dado.nextLine());
 			System.out.print("Check-out date: (dd/MM/yyyy): ");
 			checkOut = sdf.parse(dado.nextLine());
-
-			String error = reservation.updateDate(checkIn, checkOut);
-			if (error != null) {
-				System.out.println("Error in reservation: " + error);
-			} else {
-				System.out.println("Reservation: " + reservation);
-			}
+	
+			reservation.updateDate(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+	
+		} catch (ParseException e) {
+			System.out.println("Invalid date!");
+		} catch (DomainException e) {
+			System.out.println("Error in reservation "+ e.getMessage());
+		} catch (RuntimeException e) {
+			System.out.println("Unexpected error");
 		}
-
+		
 		dado.close();
-
+		
 	}
 
 }
